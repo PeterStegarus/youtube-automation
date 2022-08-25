cee707dd8b83d20cf997fe3421788be4c1e42116
PeterStegarus <stegarusp@gmail.com>
2022-08-25 19:28:02 +0300

    add download-video-from-url function

---
+++ src/scrape/downloadvideo.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch'
+import * as fs from 'node:fs';
+
+const DOWNLOAD_DIR = './videos';
+
+async function downloadVideo(url, fileName) {
+    if (!fs.existsSync(DOWNLOAD_DIR)) {
+        fs.mkdirSync(DOWNLOAD_DIR);
+    }
+
+    const path = `${DOWNLOAD_DIR}/${fileName}`;
+
+    const response = await fetch(url);
+    const arrayBuffer = await response.arrayBuffer();
+
+    fs.writeFile(path, Buffer.from(arrayBuffer), (err) => {
+        if (err) console.log(err);
+    });
+}
+
+export default downloadVideo
