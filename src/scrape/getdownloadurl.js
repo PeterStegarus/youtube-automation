d523649fb291338e5561d77cfbf7eb40482ee23c
PeterStegarus <stegarusp@gmail.com>
2022-08-25 19:29:02 +0300

    get download url from ttdownloader

---
+++ src/scrape/getdownloadurl.js
@@ -0,0 +1,38 @@
+import { default as Axios } from 'axios'
+import { load } from 'cheerio'
+import { stringify } from 'qs'
+
+const getDownloadUrl = url => {
+    return new Promise((resolve, reject) => {
+        Axios.get('https://ttdownloader.com/').then((data) => {
+            const $ = load(data.data)
+            const cookie = data.headers['set-cookie'].join('')
+            const dataPost = {
+                url: url,
+                format: '',
+                token: $('#token').attr('value')
+            }
+            Axios({
+                method: 'POST',
+                url: 'https://ttdownloader.com/query/',
+                headers: {
+                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    origin: 'https://ttdownloader.com',
+                    referer: 'https://ttdownloader.com/',
+                    cookie: cookie,
+                },
+                data: stringify(dataPost)
+            }).then(({ data }) => {
+                const $ = load(data)
+                const result = $('#results-list > div:nth-child(2) > div.download > a')?.attr('href');
+                resolve(result);
+            }).catch(e => {
+                reject({ status: false, message: 'error fetch data', e: e.message })
+            })
+        }).catch(e => {
+            reject({ status: false, message: 'error fetch data', e: e.message })
+        })
+    })
+}
+
+export default getDownloadUrl;
