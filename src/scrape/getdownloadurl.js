9eb0d816012a8895cc915fef81ed131853a581a5
PeterStegarus <stegarusp@gmail.com>
2022-08-27 14:03:17 +0300

    test workflows

--- src/scrape/getdownloadurl.js
+++ src/scrape/getdownloadurl.js
@@ -14,6 +14,7 @@ const getDownloadUrl = url => {
                 format: '',
                 token: $('#token').attr('value')
             }
+            console.log('start data post');
             Axios({
                 method: 'POST',
                 url: 'https://ttdownloader.com/query/',
