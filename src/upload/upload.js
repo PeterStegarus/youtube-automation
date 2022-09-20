8b0af4d395f219c9d839e22b48e56a6885662f29
PeterStegarus <stegarusp@gmail.com>
2022-09-20 04:18:32 +0300

    Update videos.json after each uploaded video

--- src/upload/upload.js
+++ src/upload/upload.js
@@ -20,11 +20,23 @@ const videos = scrapeData.videos.filter(video => video.uploaded == false)
 
 for (const credential of CREDENTIALS) {
     const categoryVideos = videos.filter(video => video.category == credential.category);
-    const uploadVideos = categoryVideos.map(video => ({
-        path: `${DOWNLOAD_DIR}/${video.fileName}`,
-        title: video.title,
-        description: video.title
-    }))
+    const uploadVideos = categoryVideos.map(video => {
+        const uploadVideo = {
+            path: `${DOWNLOAD_DIR}/${video.fileName}`,
+            title: video.title,
+            description: video.title
+        }
+
+        uploadVideo.onSuccess = () => {
+            console.log(`${uploadVideo.path} - Success`);
+            video.uploaded = true;
+            fs.writeFileSync(SCRAPE_DATA_PATH, JSON.stringify(scrapeData), 'utf8');
+            fs.unlinkSync(uploadVideo.path);
+            uploadVideo.path = null;
+        }
+
+        return uploadVideo;
+    })
 
     console.log(`Uploading ${uploadVideos.length} in category [${credential.category}]`);
 
@@ -34,10 +46,7 @@ for (const credential of CREDENTIALS) {
         let attempts = 1;
         while (attempts <= UPLOAD_ATTEMPTS) {
             try {
-                console.log(await upload(credential, uploadVideos, PUPPETEER_OPTIONS));
-                categoryVideos.forEach(video => video.uploaded = true)
-                uploadVideos.forEach(video => fs.unlinkSync(video.path));
-                fs.writeFileSync(SCRAPE_DATA_PATH, JSON.stringify(scrapeData), 'utf8');
+                console.log(await upload(credential, uploadVideos.filter(video => video.path !== null), PUPPETEER_OPTIONS));
                 break;
             } catch (e) {
                 console.log(`${credential.category}: ${e.message} - Attempt ${attempts} / ${UPLOAD_ATTEMPTS}`);
